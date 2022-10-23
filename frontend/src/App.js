c700386cd33742f86548ae252eaee0d52642dcfa
BlondelMbou <73158427+BlondelMbou@users.noreply.github.com>
2022-10-23 20:42:31 +0200

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shopping</a>
+      </header>
+      <main>product list</main>
+    </div>
+  );
+}
+
+export default App;
